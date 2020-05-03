fc0cbf84baa9f1063d1ccc047bdce557d1340f9a
niulake <493243584@qq.com>
2020-05-04 01:30:08 +0800

    Initial commit

---
+++ Moudle01/src/test/Test.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Test {
+    public static void main(String[] arg0) {
+        System.out.println("aaaaaaaaaaa");
+    }
+}
---
+++ src/test/SayHellow.java
@@ -0,0 +1,7 @@
+package test;
+
+public class SayHellow {
+    public static void main(String[] arg0){
+        System.out.println("hellow");
+    }
+}
